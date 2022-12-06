7e6de34076bc833d7f7680077f9c6fbca542a30d
Andrew <andrew@eseeds.com>
2022-12-06 15:30:29 +0000

    Delete list func

--- routes/shoppingList.js
+++ routes/shoppingList.js
@@ -1,18 +1,23 @@
-import express from "express";
-import { getShoppingList, postListItem } from "../models/shoppingList.js";
+import express from 'express';
+import { getShoppingList, postListItem } from '../models/shoppingList.js';
 
 const router = express.Router();
 
 /* GET shopping list. */
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
   const data = await getShoppingList();
   res.json({ success: true, payload: data });
 });
 
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
   const { listItem } = req.body;
   const result = await postListItem(listItem);
   res.status(201).json({ success: true, payload: result });
+});
+
+router.delete('/', async (req, res) => {
+  const result = await deleteAll();
+  res.status(200).json({ success: true, message: result });
 });
 
 export default router;
